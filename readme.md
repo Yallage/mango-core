a166cc527b5fa3918b7bdc5c9d1d19633e9a17e5
hanbings <3219065882@qq.com>
2022-03-16 13:37:28 +0800

    🎉 Feat: Init commit.

---
+++ readme.md
@@ -0,0 +1,2 @@
+<h1 align="center">🥭 Mango Core 芒果核心</h1>
+
---
+++ src/main/java/com/yallage/mango/core/MangoCore.java
@@ -0,0 +1,20 @@
+package com.yallage.mango.core;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class MangoCore extends JavaPlugin {
+    @Override
+    public void onLoad() {
+        // Put your code here
+    }
+
+    @Override
+    public void onEnable() {
+        // Put your code here
+    }
+
+    @Override
+    public void onDisable() {
+        // Put your code here
+    }
+}
