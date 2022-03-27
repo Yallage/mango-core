78818c36f30955fc61f2ce42d71ffc1efaca52e8
hanbings <3219065882@qq.com>
2022-03-27 19:31:33 +0800

    🐛 Fix: Fix error bungee config path.

--- src/main/java/com/yallage/mango/core/config/MangoBungeeConfiguring.java
+++ src/main/java/com/yallage/mango/core/config/MangoBungeeConfiguring.java
@@ -21,7 +21,7 @@ public class MangoBungeeConfiguring {
 
     @SuppressWarnings("ResultOfMethodCallIgnored")
     public static void loadConfig() {
-        File file = new File("plugins" + MangoBungeeCore.getInstance().getDescription().getName() + "/config.json");
+        File file = new File("plugins/" + MangoBungeeCore.getInstance().getDescription().getName() + "/config.json");
 
         if (!file.exists()) {
             try {
